1bf0cd092699d5509d9d4c6a49199d925d315910
Johnny Estilles <johnny@estilles.com>
2020-12-15 21:36:37 +0800

    adding: polling and dynamic variables

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const TEMP_MAX_ID = 344;
 const TEMP_MIN = 2900;
 const TEMP_MAX = 7000;
 
+const INTERVAL_MIN = 250;
+const INTERVAL_DEFAULT = 500;
+
 const createTemperatureLookupTable = () => {
 	const lookupTable = {};
 
@@ -27,6 +30,15 @@ function instance(system, id, config) {
 	// super-constructor
 	instance_skel.apply(this, arguments);
 
+	self.data = {
+		status: { 
+			power: null,
+			brightness: 0, 
+			temperature: 0,
+		},
+		interval: null,
+	}
+
 	self.actions(); // export actions
 
 	return self;
@@ -37,6 +49,8 @@ instance.prototype.updateConfig = function(config) {
 
 	self.config = config;
 
+	self.updateVariableDefinitions();
+	self.initPolling();
 	self.status(self.STATE_OK);
 }
 
@@ -44,6 +58,8 @@ instance.prototype.init = function() {
 	var self = this;
 	debug = self.debug;
 
+	self.updateVariableDefinitions();
+	self.initPolling();
 	self.status(self.STATE_OK);
 }
 
@@ -66,13 +82,113 @@ instance.prototype.config_fields = function () {
 			regex: self.REGEX_IP,
 			default: '192.168.1.1',
 			required: true
-		}
+		},
+		{
+			type: 'checkbox',
+			id: 'polling',
+			label: 'Enable Polling?',
+			width: 6,
+			default: true,
+		},
+		{
+			type: 'number',
+			id: 'interval',
+			label: `Polling interval in milliseconds (default: ${INTERVAL_DEFAULT}, min: ${INTERVAL_MIN})`,
+			width: 12,
+			min: INTERVAL_MIN,
+			default: INTERVAL_DEFAULT,
+			required: true,
+		},
 	]
+}
+
+// variable definitions
+instance.prototype.updateVariableDefinitions = function() {
+	const self = this;
+	self.data.variables = {};
+
+	if (!self.config.polling) {
+		self.setVariableDefinitions([]);
+		return;
+	}
+
+	self.data.variables.on = {
+		label: 'Light Power Status',
+		name: 'power',
+		getValue: (value) => POWER_VALUES[value],
+	};
+
+	self.data.variables.brightness = {
+		label: 'Light Brightness',
+        name: 'brightness',
+        getValue: (value) => Number(value),
+	};
+
+	self.data.variables.temperature = {
+		label: 'Light Temperature',
+		name: 'temperature',
+		getColorTemp: (value) => Number(convertToK(value)),
+		getValue: (value) => `${convertToK(value)}K`,
+	};
+
+	self.setVariableDefinitions(Object.keys(self.data.variables).map(name => ({ 
+		label: self.data.variables[name].label,
+		name: self.data.variables[name].name,
+	})));
+}
+
+// poll light api on an interval
+instance.prototype.initPolling = function() {
+	const self = this;
+
+	if (self.data.interval) {
+		clearInterval(self.data.interval);
+		}
+
+	if (self.config.ip && self.config.polling) {
+		const url = `http://${self.config.ip}:9123/elgato/lights`;
+
+		self.data.interval = setInterval(() => {
+			self.system.emit('rest_get', url, function (err, result) {
+				if (err !== null) {
+					self.log('error', 'HTTP GET Request failed (' + result.error.code + ')');
+					self.status(self.STATUS_ERROR, result.error.code);
+					return;
+}
+				self.updateLightStatus(result.data.lights[0]);
+				self.status(self.STATUS_OK);
+			});
+		}, self.config.interval);
+	}
+}
+
+// update data status and instance variables
+instance.prototype.updateLightStatus = function(status) {
+	const self = this;
+
+	Object.keys(self.data.variables).forEach(id => {
+		const value = status[id]
+		const name = self.data.variables[id].name;
+
+		if (self.data.status[name] !== value) {
+			self.data.status[name] = value;
+			if (typeof self.data.variables[id].getValue === 'function') {
+				this.setVariable(name, self.data.variables[id].getValue(value));
+			} else {
+				this.setVariable(name, value);
+            }
+		}
+	});
 }
 
 // When module gets deleted
 instance.prototype.destroy = function() {
 	var self = this;
+
+	if (self.data.interval) {
+		clearInterval(self.data.interval);
+	}
+
 	debug("destroy");
 }
 
@@ -166,6 +282,7 @@ instance.prototype.action = function(action) {
 				}
 				else {
 					self.status(self.STATUS_OK);
+					self.updateLightStatus(result.data.lights[0]);
 				}
 			});
 		}
