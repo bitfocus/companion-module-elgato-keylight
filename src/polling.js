10ee6b19e7d2e5b54fd66d6c25e08020be925de3
Johnny Estilles <johnny@estilles.com>
2020-12-31 00:34:28 +0800

    Fixing: syntax error

--- src/polling.js
+++ src/polling.js
@@ -13,7 +13,7 @@ module.exports = {
 				this.system.emit('rest_get', this.getUrl(), (err, result) => {
 					if (err !== null) {
 						this.log('error', `HTTP GET Request failed (${result.error.code})`);
-						this.status(thos.STATUS_ERROR, result.error.code);
+						this.status(this.STATUS_ERROR, result.error.code);
 						return;
 					}
 					this.updateVariables(result.data.lights[0]);
