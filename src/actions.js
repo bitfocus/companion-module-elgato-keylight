71713e7f4f74b98e0369df42c534eae4a9e38dff
Gwenitora <gwenitora.gardu74@gmail.com>
2025-12-04 10:16:41 +0100

    refactor: update action names and options for clarity and consistency

--- src/actions.js
+++ src/actions.js
@@ -13,11 +13,12 @@ module.exports = {
 			options: [
 				{
 					type: 'dropdown',
-					label: 'Power on/off',
+					label: 'Set Power to:',
 					id: 'bool',
 					choices: [
 						{ id: 'off', label: 'off' },
-						{ id: 'on', label: 'on' },
+                        { id: 'on', label: 'on' },
+                        { id: 'toggle', label: 'toggle' },
 					],
 					default: 'off',
 				},
@@ -26,41 +27,40 @@ module.exports = {
 				await self.runAction(action)
 			},
 		}
-		actions.powercycle = {
-			name: 'Power Cycle',
-			options: [],
-			callback: async (action) => {
-				await self.runAction(action)
-			},
-		}
 
 		actions.colortemp = {
 			name: 'Color Temperature',
 			options: [
+                {
+                    type: 'dropdown',
+                    label: 'Mode:',
+                    id: 'mode',
+                    choices: [
+                        { id: 'Set', label: 'Set to value' },
+                        { id: 'AddSub', label: 'Increase/Decrease by value' }
+                    ],
+                    default: 'Set'
+                },
 				{
 					type: 'dropdown',
-					label: 'Color Temperature',
+					label: 'Color Temperature:',
 					id: 'temp',
 					choices: this.TEMP_CHOICES,
 					default: this.MIRED_MIN,
+                    range: true,
+                    isVisible: (options) => options.mode === 'Set'
 				},
-			],
-			callback: async (action) => {
-				await self.runAction(action)
-			},
-		}
-		actions.colortempchange = {
-			name: 'Increase/Decrease Color Temperature (-200 to +200)',
-			options: [
-				{
-					type: 'number',
-					label: 'Color Temperature Delta',
-					id: 'delta',
-					min: -200,
-					max: 200,
-					default: 100,
-					required: true,
-				},
+                {
+                    type: 'number',
+                    label: 'Color Temperature Delta:',
+                    id: 'delta',
+                    min: -200,
+                    max: 200,
+                    default: 100,
+                    required: true,
+                    range: true,
+                    isVisible: (options) => options.mode === 'AddSub'
+                },
 			],
 			callback: async (action) => {
 				await self.runAction(action)
@@ -69,6 +69,16 @@ module.exports = {
 		actions.brightness = {
 			name: 'Brightness',
 			options: [
+                {
+                    type: 'dropdown',
+                    label: 'Mode:',
+                    id: 'mode',
+                    choices: [
+                        { id: 'Set', label: 'Set to value' },
+                        { id: 'AddSub', label: 'Increase/Decrease by value' }
+                    ],
+                    default: 'Set'
+                },
 				{
 					type: 'number',
 					label: 'Brightness',
@@ -78,24 +88,19 @@ module.exports = {
 					default: 50,
 					required: true,
 					range: true,
+                    isVisible: (options) => options.mode === 'Set'
 				},
-			],
-			callback: async (action) => {
-				await self.runAction(action)
-			},
-		}
-		actions.brightnesschange = {
-			name: 'Increase/Decrease Brightness (-25 to +25)',
-			options: [
-				{
-					type: 'number',
-					label: 'Brightness Delta',
-					id: 'delta',
-					min: -25,
-					max: 25,
-					default: 10,
-					required: true,
-				},
+                {
+                    type: 'number',
+                    label: 'Brightness Delta:',
+                    id: 'delta',
+                    min: -25,
+                    max: 25,
+                    default: 10,
+                    required: true,
+                    range: true,
+                    isVisible: (options) => options.mode === 'AddSub'
+                },
 			],
 			callback: async (action) => {
 				await self.runAction(action)
@@ -111,31 +116,35 @@ module.exports = {
 
 			switch (action.actionId) {
 				case 'power': {
+                    if (action.options.bool === 'toggle') {
+                        lightObj.on = 1 - this.data.status.power
+                        break
+                    }
 					lightObj.on = action.options.bool === 'on' ? 1 : 0
 					break
 				}
-				case 'powercycle': {
-					lightObj.on = 1 - this.data.status.power
-					break
-				}
 				case 'colortemp': {
-					lightObj.temperature = parseInt(action.options.temp)
-					break
+                    if (action.options.mode === 'Set') {
+                        lightObj.temperature = parseInt(action.options.temp)
+                        break;
+                    }
+                    let newTemp = getKelvin(this.data.status.temperature) + action.options.delta
+                    if (newTemp > this.KELVIN_MAX) {
+                        self.log('info', `Attempted to increase temperature beyond max value. Type: ${action.actionId}`)
+                        return
+                    }
+                    if (newTemp < this.KELVIN_MIN) {
+                        self.log('info', `Attempted to decrease temperature below max value. Type: ${action.actionId}`)
+                        return
+                    }
+                    lightObj.temperature = getMired(newTemp)
+                    break
 				}
-				case 'colortempchange': {
-					let newTemp = getKelvin(this.data.status.temperature) + action.options.delta
-					if (newTemp > this.KELVIN_MAX) {
-						self.log('info', `Attempted to increase temperature beyond max value. Type: ${action.actionId}`)
-						return
-					}
-					if (newTemp < this.KELVIN_MIN) {
-						self.log('info', `Attempted to decrease temperature below max value. Type: ${action.actionId}`)
-						return
-					}
-					lightObj.temperature = getMired(newTemp)
-					break
-				}
-				case 'brightnesschange': {
+				case 'brightness': {
+                    if (action.options.mode === 'Set') {
+                        lightObj.brightness = parseInt(action.options.brightness)
+                        break;
+                    }
 					lightObj.brightness = this.data.status.brightness + action.options.delta
 					if (lightObj.brightness > this.BRIGHTNESS_MAX) {
 						self.log('info', `Attempted to increase brightness beyond max value. Type: ${action.actionId}`)
@@ -145,11 +154,6 @@ module.exports = {
 						self.log('info', `Attempted to decrease brightness below max value. Type: ${action.actionId}`)
 						return
 					}
-					break
-				}
-				case 'brightness': {
-					let brightness = action.options.brightness
-					lightObj.brightness = parseInt(brightness)
 					break
 				}
 			}
