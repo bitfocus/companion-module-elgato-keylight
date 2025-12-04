5d38dbf9a41b341b601ca8b21b246c9e4833b4e5
Gwenitora <gwenitora.gardu74@gmail.com>
2025-12-04 10:48:07 +0100

    refactor: improve error handling and status updates in polling logic

--- src/polling.js
+++ src/polling.js
@@ -1,6 +1,8 @@
 const { InstanceStatus } = require('@companion-module/base')
 const { setIntervalAsync, clearIntervalAsync } = require('set-interval-async')
 const { got } = require('got-cjs')
+
+let lastStatus = undefined;
 module.exports = {
 	getUrl() {
 		return `http://${this.config.ip}:9123/elgato/lights`
@@ -13,20 +15,23 @@ module.exports = {
 
 		if (this.config.ip && this.config.polling) {
 			this.data.interval = setIntervalAsync(async () => {
-				try {
-					await got.get(this.getUrl(), {}).then((res) => {
-						const data = JSON.parse(res.body)
+                await got.get(this.getUrl(), { retry: { limit: 10 } }).then((res) => {
+                    const data = JSON.parse(res.body)
 
-						this.updateVariables(data.lights[0])
-						this.updateStatus(InstanceStatus.Ok)
-					})
-				} catch (error) {
-					if (error !== null) {
-						this.log('error', `HTTP GET Request failed (${error})`)
-						this.updateStatus(InstanceStatus.UnknownError, error)
-						return
-					}
-				}
+                    this.updateVariables(data.lights[0])
+                    if (lastStatus !== "ok") {
+                        this.updateStatus(InstanceStatus.Ok);
+                        lastStatus = "ok";
+                    }
+                }).catch((err) => {
+                    if (err !== null) {
+                        if (lastStatus !== "error") {
+                            this.updateStatus(InstanceStatus.UnknownError, err);
+                            lastStatus = "error";
+                        }
+                        this.log('error', `HTTP GET Request failed (${err})`);
+                    }
+                })
 			}, this.config.interval)
 		}
 	},
